e564d8c0a3d1f8e6fee3e7cfa31244e5f8624039
이소정 <720sojung@gmail.com>
2020-01-07 17:21:29 +0900

    컨트롤러 수정

--- src/main/java/com/awstest/controller/AwsController.java
+++ src/main/java/com/awstest/controller/AwsController.java
@@ -2,13 +2,15 @@ package com.awstest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class AwsController {
 	
 	@GetMapping("/")
-	public String main() {
-		return "test";
+	public ModelAndView mv() {
+		ModelAndView mv = new ModelAndView("test");
+		return mv;
 	}
 
 }
