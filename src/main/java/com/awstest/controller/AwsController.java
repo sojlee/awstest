ece78aa2e04be9f7a27379941221b8ab6822b44e
이소정 <720sojung@gmail.com>
2020-01-07 16:45:50 +0900

    초기 설정

---
+++ src/main/java/com/awstest/controller/AwsController.java
@@ -0,0 +1,13 @@
+package com.awstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AwsController {
+	
+	@GetMapping("/")
+	public String main() {
+		return "hi";
+	}
+}
